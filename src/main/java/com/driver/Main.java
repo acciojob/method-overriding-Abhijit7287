88e3c83d589b5b45fae0845cb9b787f2bb93df90
abhijit <abhijit@DESKTOP-LUQ9HF8>
2023-07-12 18:30:38 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,30 @@ package com.driver;
 
 public class Main {
   
+   public static void main (String[]args){
+     
+    Child b = new Child();
+    
+    String s = b.meth("abc");
+    
+    System.out.println(s);
+    
+    String s1 = b.meth2("abc");
+    
+     System.out.println(s1);
+       
+   }
+   public static class Parent {
+       
+      public static String meth(String s){
+       return "Invoking method from class A";
+      }
+       
+   }
+   public static class Child extends Parent
+   {
+       public static String meth2(String s){
+       return "Method is overridden in Extendend class B";
+       }
+   }
 }
