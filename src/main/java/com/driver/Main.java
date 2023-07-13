a794dc526512340830fa75f619dfee8bf4d5f25e
abhijit <abhijit@DESKTOP-LUQ9HF8>
2023-07-13 14:09:13 +0530

    fourth commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,10 +4,6 @@ public class Main {
   
    public static void main (String[]args){
      
-    A b = new B();
-    
-    System.out.println(b.meth());
-    
     B a = new B();
     
     System.out.println(a.meth());
